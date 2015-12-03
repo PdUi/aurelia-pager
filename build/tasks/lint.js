da9bcb2c2b35f9c250e42e7dc98f0b6e3f6724d8
PeinearyDevelopment <peinearydevelopment@gmail.com>
2015-12-03 13:45:09 -0500

    added css linting

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,11 +1,29 @@
 var gulp = require('gulp');
 var paths = require('../paths');
 var eslint = require('gulp-eslint');
+var runSequence = require('run-sequence');
+var csslint = require('gulp-csslint');
+var sass = require('gulp-sass');
 
 // runs eslint on all .js files
-gulp.task('lint', function() {
+gulp.task('lint-js', function() {
   return gulp.src(paths.source)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
+});
+
+gulp.task('lint-css', function() {
+    return gulp.src(paths.scss)
+      .pipe(sass())
+      .pipe(csslint())
+      .pipe(csslint.reporter());
+})
+
+gulp.task('lint', function(callback) {
+  return runSequence(
+    'lint-js',
+    'lint-css',
+    callback
+  );
 });
